4ac66c46587175b81492cec8ba290ad14aa85d7d
Soyas Limbu <soyaslimbu07@gmail.com>
2023-06-21 15:04:49 +1000

    fixes #7

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react'
-import Logo from './components/Logo/Logo'
-import Button from './components/Button/Button'
-import Text from './components/Text/Text'
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
     return (
         <div>
             <section className='main-container'>
-                {/* <Logo type='primary' /> */}
-                {/* <Button
-                    title='Sign Up'
-                    width="9rem"
-                    varaint='secondary'
-                /> */}
-                <Text
+                <Navbar />
+                {/* <Text
                     text='lorem'
                     variant='h1'
-                />
+                /> */}
             </section>
         </div>
     )
